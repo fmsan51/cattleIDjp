bf4fb3b950c821956b09c4025d4f954c042171b0
Y Fujimoto <fmsan51@gmail.com>
2023-12-14 20:06:58 +0900

    Improve locale setting

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 .cid_locale <- Sys.getlocale("LC_CTYPE")
 
 .onLoad <- function(libname, pkgname) {
-  Sys.setlocale("LC_CTYPE", "Japanese")
+  Sys.setlocale("LC_CTYPE", "ja_JP.UTF-8")
 }
 
 .onUnload <- function(libname, libpath) {
